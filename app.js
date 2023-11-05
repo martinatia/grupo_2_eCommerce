ee21a3751be9c13d6d4a47beabd9c24b8da6ca1d
Martin Atia <atia.walter.martin@gmail.com>
2023-11-05 11:46:00 -0300

    Se expuso la carpeta publc, ruteo  a las distintas paginas, y estructuras vacias de los html

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+app.use(express.static("public"));
 
 app.listen(3000, () => console.log("Servidor corriendo"));
 
 //Sistema de ruteo
-app.get("/", (req, res) => res.send("Pantalla Principal"));
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("./views/index.html"));
+});
+
 app.get("/carrito", (req, res) => {
-  res.send("Carrito"); 
+  res.sendFile(path.resolve("./views/carrito.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve("./views/login.html"));
+});
+
+app.get("/product_description", (req, res) => {
+  res.sendFile(path.resolve("./views/product_description.html"));
+});
+
+app.get("/registration", (req, res) => {
+  res.sendFile(path.resolve("./views/registration.html"));
 });
