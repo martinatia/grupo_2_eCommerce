9d81026ccc6fdd1b202d9ddf8845fadb826c2b92
Jose Marcelo Lodi <108854222+GuiBen7@users.noreply.github.com>
2023-12-06 12:55:25 -0300

    Revert "Crear rutas"
    This reverts commit f4243d14419301bcfe503aca1270db85e3946861.

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -1,15 +1,1 @@
 /* Responsabilidad de Lodi */
-const {Router} = require('express');
-const controller = require('../controllers/mainController');
-const router = Router();
-
-
-const routes = {
-    home: '/',
-    productDescription: '/productDescription',
-    carrito: '/carrito',
-}
-
-router.get(routes.home, controller.index);
-router.get(routes.productDescription, controller.productDescription);
-routes.get(routes.carrito, controller.carrito);
