f4243d14419301bcfe503aca1270db85e3946861
Jose Marcelo Lodi <108854222+GuiBen7@users.noreply.github.com>
2023-12-06 01:48:26 -0300

    Crear rutas

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,1 +1,17 @@
 /* Responsabilidad de Lodi */
+const {Router} = require('express');
+const router = Router();
+const controller = require('../controllers/userController')
+
+
+const routes = {
+    login: '/login',
+    registration: '/register',
+}
+
+router.get(routes.login, controller.login)
+router.get(routes.registration, controller.registration)
+
+
+
+module.exports = router;
