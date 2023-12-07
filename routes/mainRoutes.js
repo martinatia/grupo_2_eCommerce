fe06e16f0340ef85c15fbeb8ed237a3559a894e7
Jose Marcelo Lodi <108854222+GuiBen7@users.noreply.github.com>
2023-12-07 01:02:10 -0300

    agregado de ruta de modificacion de producto

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -8,13 +8,15 @@ const routes = {
     home: '/',
     productDescription: '/productDescription',
     carrito: '/carrito',
-    nuevoProducto: '/nuevo-producto'
+    nuevoProducto: '/nuevo-producto',
+    modifyProducts: '/producto-modificacion',
 }
 
 router.get(routes.home, controller.index);
 router.get(routes.productDescription, controller.productDescription);
-routes.get(routes.carrito, controller.carrito);
-routes.get(routes.nuevoProducto, controller.nuevoProducto);
+router.get(routes.carrito, controller.carrito);
+router.get(routes.nuevoProducto, controller.nuevoProducto);
+router.get(routes.modifyProducts, controller.modifyProducts);
 
 
 module.exports = router;
