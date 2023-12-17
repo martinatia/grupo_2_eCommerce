a16aa1dbe1ceff1ade3a5502653aa7553df0cfbe
Martin Atia <atia.walter.martin@gmail.com>
2023-12-16 23:01:34 -0300

    modify-product creado y mostrado dinamicamente
    Casi listo para empezar con los verbos http

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,7 +1,17 @@
 /* Responsabilidad de Martin*/
+const fs = require ('fs');
+const path = require('path');
+const productsFilePath = path.join(__dirname, '../data/products.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
 const controller = {
     index: (req,res) => {
-        res.render('products/index');
+        const inSaleProducts = products.filter((product) => product.section=="in-sale");
+        const featuredProducts = products.filter((product) => product.section == "featured");
+        const lastColectionProducts = products.filter((product) => product.section == "last-colection");
+        res.render('products/index', {inSaleProducts, featuredProducts, lastColectionProducts});
     },
     productDescription:(req, res) =>{
         res.render('products/product-description');
@@ -13,8 +23,8 @@ const controller = {
         res.render('products/create-product');
     },
     modifyProduct: (req,res) =>{
-        console.log(req.params.id);
-        res.render('products/modify-product')
+        const producto = products.find((producto) => producto.id == req.params.id);
+        res.render('products/modify-product', {producto})
     },
 }
 module.exports = controller;
