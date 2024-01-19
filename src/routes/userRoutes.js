7a19ce7171f798e5ccece6b184b744497dd5dc37
luana32 <102762121+luana32@users.noreply.github.com>
2024-01-19 15:44:11 -0300

    Act 5 (falta ver errores)

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,6 +2,21 @@
 const {Router} = require('express');
 const router = Router();
 const controller = require('../controllers/userController')
+const multer = require('multer');
+const path = require('path');
+
+const multerDiskStorage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        const folder = path.join(__dirname, "../../public/img/userImageProfile");
+        callback(null, folder);
+    },
+    filename: (req, file, callback) => {
+        let imageName = Date.now() + path.extname(file.originalname);
+        callback(null, imageName);
+    }
+});
+
+const fileUpload = multer({ storage: multerDiskStorage });
 
 const routes = {
     login: '/login',//creo que va en el main controller
@@ -9,11 +24,10 @@ const routes = {
     //TODO: MOVER AL MAIN
     /* aqui iria otra pagina como editUser, que reciba un id/edit */
     shoppingCart: '/:id/shopping-cart'
-
 }
 
 router.get(routes.login, controller.login);
-router.get(routes.registration, controller.registration);
+router.post(routes.registration, fileUpload.single('image'), controller.registration);
 router.get(routes.shoppingCart, controller.shoppingCart);
 
 
