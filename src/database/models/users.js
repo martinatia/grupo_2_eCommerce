0c3d14c6e06adea76528130fb35a07cb8f10dbcd
Jose Marcelo Lodi <108854222+GuiBen7@users.noreply.github.com>
2024-04-03 18:23:17 -0300

    configuracion sequelize

--- src/database/models/users.js
+++ src/database/models/users.js
@@ -2,41 +2,41 @@ module.exports = (sequelize, dataTypes) => {
     let alias = 'users';
     let cols = {
         user_id: {
-            type: DataTypes.INTEGER,
+            type: dataTypes.INTEGER,
             primaryKey: true,
             autoIncrement: true
         },
         first_name: {
-            type: DataTypes.STRING(100),
+            type: dataTypes.STRING(100),
             allowNull: false
         },
         last_name: {
-            type: DataTypes.STRING(100),
+            type: dataTypes.STRING(100),
             allowNull: false
         },
         address: {
-            type: DataTypes.STRING(100),
+            type: dataTypes.STRING(100),
             allowNull: false
         },
         password: {
-            type: DataTypes.STRING(250),
+            type: dataTypes.STRING(250),
             allowNull: false
         },
         email: {
-            type: DataTypes.STRING(100),
+            type: dataTypes.STRING(100),
             allowNull: false
         },
         profile_image_url: {
-            type: DataTypes.STRING(200),
+            type: dataTypes.STRING(200),
             allowNull: false
         },
         user_type: {
-            type: DataTypes.STRING(15),
+            type: dataTypes.STRING(15),
             allowNull: false,
             defaultValue: 'Cliente'
         },
         date_created: {
-            type: DataTypes.DATE,
+            type: dataTypes.DATE,
             allowNull: false
         }
     };
